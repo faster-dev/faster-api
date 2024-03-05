797e2822dc8eea5a8be3fe76442d29d8024c0a2b
Nicholas Piano <nicholas.d.piano@gmail.com>
2024-03-05 13:13:38 +0100

    chore: import tests

--- migrate.ts
+++ migrate.ts
@@ -1,1 +1,12 @@
-console.log('MIGRATE', process.env.DATABASE_CONNECTION_STRING);
+import { pgTable, serial, text, varchar } from 'drizzle-orm/pg-core';
+import { drizzle } from 'drizzle-orm/node-postgres';
+import { Client } from 'pg';
+
+import { handler } from './netlify/functions/api';
+import { DB } from './netlify/functions/db';
+
+const client = new Client(process.env.DATABASE_CONNECTION_STRING);
+await client.connect();
+const db = drizzle(client);
+
+console.log('MIGRATE', db, handler, DB);
--- netlify/functions/api.ts
+++ netlify/functions/api.ts
@@ -1,9 +1,19 @@
 import express, { Router } from 'express';
 import serverless from 'serverless-http';
-// import pg from 'pg';
+import { drizzle } from 'drizzle-orm/node-postgres';
+import { Client } from 'pg';
 
-// var db = new pg.Client(DATABASE_CONNECTION_STRING);
-// db.connect();
+import { DB } from './db';
+
+console.log('DB', DB);
+
+const useDatabase = async () => {
+  const client = new Client(process.env.DATABASE_CONNECTION_STRING);
+  await client.connect();
+  const db = drizzle(client);
+
+  return db;
+};
 
 const api = express();
 
