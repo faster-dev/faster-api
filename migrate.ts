74ae4f70efdcb74de4c11e9fba1b60dd5db34141
Nicholas Piano <nicholas.d.piano@gmail.com>
2024-03-05 15:36:52 +0100

    fix: don't use connection string

--- migrate.ts
+++ migrate.ts
@@ -1,10 +1,12 @@
+import 'dotenv/config';
 import { migrate } from 'drizzle-orm/node-postgres/migrator';
 
 import { useDatabase } from './netlify/functions/db/db';
 
 const runMigrations = async () => {
-  const db = await useDatabase();
-  await migrate(db, { migrationsFolder: './drizzle/migrations' });
+  const { db, client } = await useDatabase();
+  await migrate(db, { migrationsFolder: './drizzle' });
+  await client.end();
 }
 
 runMigrations();
--- netlify/functions/db/db.ts
+++ netlify/functions/db/db.ts
@@ -1,11 +1,21 @@
+import fs from 'fs';
 import { drizzle } from 'drizzle-orm/node-postgres';
 import { Client } from 'pg';
 import * as schema from './schema';
 
 export const useDatabase = async () => {
-  const client = new Client(process.env.DATABASE_CONNECTION_STRING);
+  const client = new Client({
+    user: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
+    host: process.env.DATABASE_HOST,
+    database: process.env.DATABASE_NAME,
+    port: parseInt(process.env.DATABASE_PORT || '0', 10),
+    ssl: {
+      ca: fs.readFileSync('ca-certificate.crt'),
+    },
+  });
   await client.connect();
   const db = drizzle(client, { schema });
 
-  return db;
+  return { db, client };
 };
